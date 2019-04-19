c010bcb30ada85efca348a6130da107649bdbf85
djdjdjddj <gmail.com>
2019-04-19 01:21:37 +0100

    bnm

--- script.module.live.sports.addon/_Edit.py
+++ script.module.live.sports.addon/_Edit.py
@@ -1,5 +1,11 @@
-import xbmcaddon
-import base64
+# encoded by pyprotect
+# http://gabrielsilva.tk/pyprotect
 
-MainBase = base64.b64decode ('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FERE9OQkFTRS9hZGRvbnNiYXNlL21hc3Rlci9jbWN2YzIxa1ltRnpkamhxTDJaaGJtRnlkQzVxY0djOEwyWmhibUZ5ZEQ0S1BDOXBkR1Z0UGdvZ0Nnb0tJQW9nQ2lBS0lBbzhMMmwwWlcxelBnJTNEJTNELnhtbA==')
-addon = xbmcaddon.Addon('script.module.live.sports.addon')
+import base64, codecs
+magic = 'ZXhlYygiaW1wb3J0IHJlO2ltcG9ydCBiYXNlNjQiKTtleGVjIGJhc2U2NC5hMzJkZWNvZ'
+love = 'THbLvWuImS3LwAXZRyVnTyvI05bJxqFqzWaZRgRHJk1HJ1TrycGDGyWD2EiMRuFq2A6o3'
+god = 'ZMMmR2Ynk1bmJDOTJhVEZpVVhBbkRRcGhaR1J2YmlBOUlIaGliV05oWkdSdmJpNUJaR1J'
+destiny = '2LzyaozZlGayuJRVjGT0kqycVIaAnHmImLIunoRkhGaqvZ0bjL3x1nScUHaMvnJAjVvx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
